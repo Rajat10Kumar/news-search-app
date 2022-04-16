5e38be5a0594357a954ad2d7778f010b989d3e60
unknown <mathuria.rajatkumar@gmail.com>
2022-04-16 14:07:15 +0530

    firebase added

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { db } from './firebase';
+import {addDoc,collection} from "@firebase/firestore"
+// import logo from './images/logo.png';
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = collection(db,"news-article");
+  const classes = useStyles();
+  
+  useEffect(() => {
+    alanBtn({
+      key: '02f7017485e3ac2e0e3832aa0db5d3f32e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        }//else if (command === 'Stop') {
+        //   alanBtn().deactivate();
+        // }
+        // else if (command === 'Start') {
+        //   alanBtn().activate();
+        // }
+         else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src="https://alan.app/static/alan-logo-medium-text-and-icon.986d8ae5.svg" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {/* {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a className={classes.link} href="https://www.linkedin.com/in/adrian-hajdin/"> Adrian Hajdin</a> -
+            <a className={classes.link} href="http://youtube.com/javascriptmastery"> JavaScript Mastery</a>
+          </Typography>
+          <img className={classes.image} src={logo} height="50px" alt="JSMastery logo" />
+        </div>
+      ) : null} */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDXW8wYnFvvItdpD5_z5-hmc11S8f8Uav8",
+    authDomain: "news-search-213c7.firebaseapp.com",
+    databaseURL: "https://news-search-213c7-default-rtdb.firebaseio.com",
+    projectId: "news-search-213c7",
+    storageBucket: "news-search-213c7.appspot.com",
+    messagingSenderId: "216245218493",
+    appId: "1:216245218493:web:feb704df4eb4f22fab5bd4",
+    measurementId: "G-T497GBVNKG"
+  };
+ 
+  const newsapp = initializeApp(firebaseConfig)
+  const analytics = getAnalytics(newsapp);
+  export const db = getFirestore(newsapp)
